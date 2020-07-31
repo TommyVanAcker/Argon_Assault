ecb73dd0007d392d733bd1664792e43fa05e612c
tommy <tommy.van.acker@gmail.com>
2020-07-31 18:27:49 +0200

    Initial commit

---
+++ Assets/LevelLoader.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+
+    static GameObject myGameObject;
+
+    float loadDelay = 5f;
+
+    private void Awake()
+    {
+        if (!myGameObject)
+        {
+            myGameObject = this.gameObject;
+            DontDestroyOnLoad(gameObject);
+        } else
+        {
+            Destroy(gameObject);
+            gameObject.SetActive(false);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(LoadGameScene());
+        
+    }
+
+    IEnumerator LoadGameScene()
+    {
+        yield return new WaitForSeconds(loadDelay);
+        int sceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(sceneIndex + 1);
+        
+    }
+}
