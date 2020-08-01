495b69d2237abd8f8684c0ecefb5a9952767f672
tommy <tommy.van.acker@gmail.com>
2020-08-01 12:46:52 +0200

    rotation in relation to position

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,11 +7,26 @@ public class Player : MonoBehaviour
 {
     [Header("General Settings")]
     [Tooltip("in ms^-1")]
-    [SerializeField] float xSpeed = 20f;
+    [SerializeField] float speed = 20f;
+
     [Tooltip("in m")]
-    [SerializeField] float XminScreenPos = -10f;
+    [SerializeField] float xMinScreenPos = -10f;
     [Tooltip("in m")]
-    [SerializeField] float XmaxScreenPos = 10f;
+    [SerializeField] float xMaxScreenPos = 10f;
+    [Tooltip("in m")]
+    [SerializeField] float yMinScreenPos = -7f;
+    [Tooltip("in m")]
+    [SerializeField] float yMaxScreenPos = 8f;
+
+    [Header("Player Play Area")]
+    [SerializeField] float positionPitchFactor = -4f;
+    [SerializeField] float controlPitchFactor = -16f;
+    [SerializeField] float positionYawFactor = 4f;
+    [SerializeField] float controlRollFactor = -15f;
+
+
+    float xThrow, yThrow;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,11 +36,39 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float xThrow = Input.GetAxis("Horizontal");
-        float xOffsetThisFrame = xThrow * xSpeed * Time.deltaTime;
-        float rawNewLocalXPosition = transform.localPosition.x + xOffsetThisFrame;
-        float clampedXPos = Mathf.Clamp(rawNewLocalXPosition, XminScreenPos, XmaxScreenPos);
-        transform.localPosition = new Vector3(clampedXPos, transform.localPosition.y, transform.localPosition.z); 
-        
+        ProcessTranslation();
+        ProcessRotation();
+
+    }
+
+    private void ProcessRotation()
+    {
+        float pitchDueToPosition = (transform.localPosition.y * positionPitchFactor);
+        float pitchDueToControlThrow = (yThrow * controlPitchFactor);
+
+        float pitch = pitchDueToPosition + pitchDueToControlThrow; 
+        float yaw = transform.localPosition.x * positionYawFactor;
+        float roll = xThrow * controlRollFactor;
+
+        Vector3 shipRotation = new Vector3(pitch, yaw, roll);
+        transform.localRotation = Quaternion.Euler(shipRotation);
+
+    }
+
+    private void ProcessTranslation()
+    {
+        xThrow = Input.GetAxis("Horizontal");
+        yThrow = Input.GetAxis("Vertical");
+
+        float xOffsetThisFrame = xThrow * speed * Time.deltaTime;
+        float yOffsetThisFrame = yThrow * speed * Time.deltaTime;
+
+        float rawlocalXPos = transform.localPosition.x + xOffsetThisFrame;
+        float rawLocalYPos = transform.localPosition.y + yOffsetThisFrame;
+
+        float clampedXPos = Mathf.Clamp(rawlocalXPos, xMinScreenPos, xMaxScreenPos);
+        float clampedYPos = Mathf.Clamp(rawLocalYPos, yMinScreenPos, yMaxScreenPos);
+
+        transform.localPosition = new Vector3(clampedXPos, clampedYPos, transform.localPosition.z);
     }
 }
