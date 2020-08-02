54f36c386166dd804aba4006b1dcf3945f4d9739
tommy <tommy.van.acker@gmail.com>
2020-08-02 14:37:51 +0200

    collisions & sendmessage between components

---
+++ Assets/Enemy.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        //Debug.Log(other.gameObject.name + "collided with enemey");
+        
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        //Debug.Log(other.gameObject.name + "triggered enemy");
+    }
+}
