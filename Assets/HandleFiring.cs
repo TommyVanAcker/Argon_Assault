91726166e7b7a01d430ec3b4ca66bd9054678c57
tommy <tommy.van.acker@gmail.com>
2020-08-03 17:06:50 +0200

    Fire on input

---
+++ Assets/HandleFiring.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandleFiring : MonoBehaviour
+{
+    [SerializeField] GameObject[] bullets;
+    bool isControlEnabled = true;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isControlEnabled)
+        {
+            ProcessFiring();
+        }
+        
+    }
+
+    private void ProcessFiring()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            Fire(true);
+        }
+        else
+        {
+            Fire(false);
+
+        }
+    }
+
+    private void Fire(bool fire)
+    {
+        foreach (GameObject bullet in bullets)
+        {
+            var bulletModule = bullet.GetComponent<ParticleSystem>().emission;
+            bulletModule.enabled = fire;
+        }
+    }
+
+    void OnPlayerDeath()
+    {
+        isControlEnabled = false;
+    }
+}
