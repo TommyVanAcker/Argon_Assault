3af4bd7462b6e4d849a3cba7369820c97c119ab3
tommy <tommy.van.acker@gmail.com>
2020-08-03 10:20:14 +0200

    Addcomponents at runtime, explosions on enemy

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] GameObject deathFX;
+    [SerializeField] Transform enemyExpParent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        AddNonTriggerCollider();
+    }
+
+    private void AddNonTriggerCollider()
+    {
+        BoxCollider boxCollider = gameObject.AddComponent<BoxCollider>();
+        boxCollider.isTrigger = false;
+    }
+
+    private void OnParticleCollision(GameObject other)
+    {
+        Instantiate(deathFX, gameObject.transform.position, Quaternion.identity, enemyExpParent);
+        Destroy(gameObject);
+    }
+}
